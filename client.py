517f881109387e4ad9a086c09de22702003d2864
NTKaworu <mf.lollerino@gmail.com>
2022-02-09 01:22:33 +0100

    yas sono  stanco

--- client.py
+++ client.py
@@ -1,11 +1,37 @@
-import socket
+import socket, sys
+IP = "192.168.1.100"
+PORT = 6060
+s = socket.socket()
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+def comunication(s):
+    while True:
+        command = input("--> ")
+        if command == "ESC":
+            s.close()
+            sys.exit()
+        else:
+            s.send(command.encode())
+            print("messaggio mandato")
+            data = s.recv()
+            print(str(data, "utf-8"))
 
-print('Received', repr(data))
+
+def connectToServer(server_ip):
+    try:
+        s.connect(server_ip)
+        print(f"connection established with: {server_ip}")
+
+    except socket.error as error:
+        print(f"Couldn't connect to server: {server_ip}'")
+        print(error)
+        print("----------------------------------------------------------------")
+        print("try again? (yes/no)")
+        r = input().lower()
+        if r == "yes":
+            connectToServer(server_ip)
+        sys.exit(1)
+    comunication(s)
+
+if __name__ == "__main__":
+    connectToServer((IP, PORT))
--- server.py
+++ server.py
@@ -1,16 +1,46 @@
-import socket
+from asyncio.subprocess import STDOUT
+import socket, subprocess
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 6060
+s = socket.socket()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+
+def comunication(conn):
+    while True:
+        #print("4")
+        #data = conn.recv(4096)
+        #print("1")
+        #command = subprocess.run(data.decode(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #print("2")
+        #command_to_send = command.stdout + command.stderr
+        #print("3")
+        #conn.send(command_to_send)
+
+        data = conn.recv(4096)
+        if data.decode() == "test":
+            conn.send(data)
+
+
+
+def server(address, backlog=1):
+    try:
+        s.bind(address)
+        s.listen(backlog)
+        print(f"Server initialized, listening on: {backlog}")
+
+    except socket.error as error:
+        print(f"Couldn't initialize connection...")
+        print(error)
+        print("----------------------------------------------------------------")
+        print("reinitializing connection...")
+        server(address, backlog=1)
+
+    conn, client_addr = s.accept()
+    print("----------------------------------------------------------------")
+    print(f"Connection established with host: {client_addr}")
+    print("----------------------------------------------------------------")
+    comunication(conn)
+
+
+if __name__ == "__main__":
+    server(("", PORT))
